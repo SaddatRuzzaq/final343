2247a4e1a15326294ef71ea8d3fc7aac2b57cbce
Saddat Ruzzaq <saddat.ruzzaq96@qmail.cuny.edu>
2025-05-12 20:35:56 -0400

    initial

--- src/app/components/Menubar.jsx
+++ src/app/components/Menubar.jsx
@@ -2,21 +2,22 @@ import Link from 'next/link'
 
 export default function Menubar() {
     return (
-     <>
-      <ul>
-        <li>
-         <Link href= '/' >Home</Link>
-        </li>
-        <li>
-          <a href='https://exam1part1.vercel.app/' target= '_blank' >Midterm Part I</a>
-        <li>
-       </li>
-         <a href= 'https://exam-1-part2.vercel.app/' target= '_blank' >Midterm Part II</a>
-         </li>
-         <li>
-          <Link href='/faq'>FAQ</Link>
-         </li>
-      </ul>
+      <>
+        <ul>
+          <li>
+            <Link href='/'>Home</Link>
+          </li>
+          <li>
+            <a href='https://https://exam1part1.vercel.app/' target='_blank'>Midterm Part I - Expense</a>
+          </li>
+          <li>
+            <a href='https://exam-1-part2.vercel.app/' target='_blank'>Midterm Part II Site</a>
+          </li>
+          <li>
+            <Link href='/faq'>FAQ</Link>
+          </li>
+        </ul>
       </>
-    );
+    )
   }
+  
